63ff5a30d13990fa553e835dc37d0f3f075875d7
Александр <perchatki79@gmail.com>
2022-12-28 18:28:03 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,75 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int clientOS=1;
+        if (clientOS==0){
+            System.out.println("Установите версию приложения для iOS по ссылке");}
+        if (clientOS==1){
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
+
+
+    public static void task2() {
+        System.out.println("Задача 2");
+
+        int clientDeviceYear=2014; //год изготовления телефона
+        int clientOS=0; // тип ОС 0=iOs; 1-Android
+
+        if (clientDeviceYear<2015 && clientOS==0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        else if (clientDeviceYear<2015 && clientOS==1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+        else if (clientOS==0){
+            System.out.println("Установите версию приложения для iOS по ссылке");}
+        else if (clientOS==1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+
+    }public static void task3() {
+        System.out.println("Задача 3");
+        int year=2024; //год, который хотим проверить
+
+        if ((year%400==0) || (year%4==0 && year%100!=0)) {
+            System.out.println(year+" год является високосным");
+        }
+
+        else {
+            System.out.println(year+" год не является високосным");
+        }
+
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+        int deliveryDistance = 95;
+        int dostavka=0;
+        if (deliveryDistance<20){
+            System.out.println("Потребуется "+(dostavka+1)+ " день");
+        }else if (deliveryDistance>20 && deliveryDistance<60){
+            System.out.println("Потребуется "+(dostavka+2)+ " дня");
+        }else if (deliveryDistance>60 && deliveryDistance<100){
+            System.out.println("Потребуется "+(dostavka+3)+ " дня");
+        }
+
+
+
+    }public static void task5() {
+        System.out.println("Задача 5");
+
+
     }
 }
